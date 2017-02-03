80a087c2f24b6d63f6fe552a595d93f441f27589
AvaHenriksen <ava.henriksen@gmail.com>
2017-02-03 11:13:13 -0600

    Details working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,20 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def new_form
+
+    render("new_form.html.erb")
+  end
+
+  def show
+    p = Photo.find(params[:id])
+    @photo = p.source
+    @caption = p.caption
+    render("show.html.erb")
+  end
+
+  def create_row
+    render("create_row.html.erb")
+  end
 
 end
